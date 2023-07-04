58bd9390f5e1b318c89321d5e62bce18e4d72e02
kingkazuma7 <your.nasionasio7@gmail.com>
2023-07-04 12:45:08 +0900

    クエリビルダ,ファサード

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,17 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 // Model読み込み
 use App\Models\Test;
+// クエリビルダ(sqlみたいなもの)
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     // ②コントローラー
     public function index()
     {
-        $values = Test::all(); // 全件（エロクアント）
+        // 全件（エロクアント） ※クエリビルダより主流
+        $values = Test::all(); // モデル名::メソッド
         $count = Test::count();
         $first = Test::findOrFail(1);
         $whereBBB = Test::where('text', '=', 'bbb')->get();
-        dd($values, $count, $first, $whereBBB);
+        
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'aaa')
+        ->select('id', 'text')
+        ->get(); // get型でとれる
+        
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
         // ddd($values, $count, $first, $whereBBB);
         return view('tests.test', compact('values')); // フォルダ/ファイル名
     }
